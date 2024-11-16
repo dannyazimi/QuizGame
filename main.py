9a9dff6aaf5db72c86b70e029442fe8ae2003440
Danny Azimi <k6m3lo@gmail.com>
2024-11-16 12:20:35 -0800

    Added data from opentdb.com. Pushing to update the Git

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank=[]
 
 for question in question_data:
-    q_text = question["text"]
-    q_answer = question["answer"]
+    q_text = question["question"]
+    q_answer = question["correct_answer"]
     new_question = Question(q_text,q_answer)
     question_bank.append(new_question)
 
